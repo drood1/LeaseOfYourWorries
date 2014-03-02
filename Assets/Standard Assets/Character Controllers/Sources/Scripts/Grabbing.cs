ad51cad7efee780933338616ffe9c7a399dd613e
mocchii <d.dan342@gmail.com>
2014-03-02 16:58:21 -0500

    hi

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/Grabbing.cs
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/Grabbing.cs
@@ -3,13 +3,61 @@ using System.Collections;
 
 public class Grabbing : MonoBehaviour {
 	public GameObject Weapon = null;
+	public bool holdingitem = false;
+	GameObject hand;
+	GameObject finger1;
+	GameObject finger2;
+	GameObject finger3;
+	GameObject finger4;
+	GameObject finger5;
+	GameObject wep;
 	// Use this for initialization
 	void Start () {
-	
+		hand = GameObject.FindWithTag("Hand");
+		finger1 = GameObject.FindWithTag("finger1");
+		finger2 = GameObject.FindWithTag("finger2");
+		finger3 = GameObject.FindWithTag("finger3");
+		finger4 = GameObject.FindWithTag("finger4");
+		finger5 = GameObject.FindWithTag("finger5");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+	}
+	public void holding(bool a){
+		holdingitem = a;
+	}
+	public void Equip(int wepnum){
+		Debug.Log(wepnum);
+		if(holdingitem == false){
+			holdingitem = true;
+		}else{
+			if(wepnum == 0){
+				
+			}else{
+				hand.transform.DetachChildren();
+				if(wepnum == 1){
+					wep = GameObject.Find("weaponPoker/jnt_poker");
+					wep.transform.parent = GameObject.Find("weaponPoker").transform;
+					SphereCollider sphere = wep.GetComponent<SphereCollider>();
+					sphere.enabled = true;
+				}
+				if(wepnum == 2){
+					wep = GameObject.Find("weaponBucket/jnt_bucket");
+					wep.transform.parent = GameObject.Find("weaponBucket").transform;
+					SphereCollider sphere = wep.GetComponent<SphereCollider>();
+					sphere.enabled = true;
+				}
+				wep.collider.enabled = true;
+				finger1.transform.parent = hand.transform;
+				finger2.transform.parent = hand.transform;
+				finger3.transform.parent = hand.transform;
+				finger4.transform.parent = hand.transform;
+				finger5.transform.parent = hand.transform;
+				//wep.transform.parent = null;
+				}
+
+		}
 	}
 }
--- Assets/Standard Assets/Character Controllers/Sources/Scripts/ItemWarp.cs
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/ItemWarp.cs
@@ -6,15 +6,19 @@ public class ItemWarp : MonoBehaviour {
 	GameObject hand;
 	GameObject wep;
 	public int wepnum = 0;
+	int prewepnum = 0;
+	Grabbing whatdo;
 	// Use this for initialization
 	void Start () {
 		hand = GameObject.FindWithTag("Hand");
 		if(wepnum == 1){
-			wep = GameObject.Find("fireplacePoker/joint_poker");
+					wep = GameObject.Find("weaponPoker/jnt_poker");
 		}
 		if(wepnum == 2){
-			wep = GameObject.Find("woodenBucket");
+			wep = GameObject.Find("weaponBucket/jnt_bucket");
 		}
+		prewepnum = wepnum;
+
 	}
 	
 	// Update is called once per frame
@@ -22,14 +26,21 @@ public class ItemWarp : MonoBehaviour {
 
 	}
 	void OnMouseOver (){
-		if(Input.GetMouseButtonDown(1)){
+		//if(whatdo.holdingitem == false){
+
+
+			if(Input.GetMouseButtonDown(1)){
+				
+				player.SendMessage("Equip",prewepnum);
+				wep.collider.enabled = false;
+				//player.gameObject.GetComponent<Grabbing>().holding(true);
 			//gameObject.transform.parent = player.transform.Find("Bip001/Bip001 Spine/Bip001 Spine1/Bip001 Neck/Bip001 R Clavicle/Bip001 R UpperArm/Bip001 R Forearm/Bip001 R Hand/Bip001 R Hand").transform;
-			wep.transform.parent = hand.transform;
-			wep.transform.localPosition = new Vector3(0,0,0);
+				wep.transform.parent = hand.transform;
+				wep.transform.localPosition = new Vector3(0,0,0);
 			//this.transform.localRotation = Quaternion.identity;
 			//this.transform.localRotation = Quaternion.Euler(0, 0, -50);
 			//gameObject.transform.parent.gameObject;
-		}
+			}
 
+		}
 	}
-}

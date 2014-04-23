a53548ccbc45b3523ff62fed94846f55b9fc85ac
eduardojfp <eduardojfp@yahoo.com>
2014-04-23 01:19:11 -0400

    Fixed Dustin's Distance Function

---
+++ Assets/Scripts/KeyEnemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This enemy wil drop a key to open a door, allowing the player to move on more quickly
+// This enemy will move erratically to increase difficulty of killing it
+public class KeyEnemy : MonoBehaviour {
+
+	float move_speed = 2f;		// Movement Speed
+	float health = 10f; 		// Will take normal damage + 1
+	float time_hit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

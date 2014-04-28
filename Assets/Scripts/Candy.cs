1de6ba9523ac2b110770470c02c6ae8b3cc11be1
eduardojfp <eduardojfp@yahoo.com>
2014-04-28 13:45:47 -0400

    Candies now have different prefabs. Candy now spins!!!

--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -11,7 +11,7 @@ public class Candy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.Rotate(Vector3.up, Time.deltaTime * 60);
 	}
 
 	private void OnTriggerEnter(Collider c){

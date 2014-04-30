31fadde5c363f8bd435abc53c6928d832992b61a
Karl Olsen <olsenk2@rpi.edu>
2014-04-30 14:11:34 -0400

    What is this? A mansion for ants?! (resized furniture)

--- Assets/Scripts/TalkToBoo.cs
+++ Assets/Scripts/TalkToBoo.cs
@@ -16,36 +16,53 @@ public class TalkToBoo : MonoBehaviour {
 	public Texture2D text_box_2;
 	
 	bool toopoor = false;
+	private double timer;
+	private bool isTiming = false;
+	
+
+	void beginTimer()	{
+		timer = 0;
+		isTiming = true;
+		while(timer < 3000)	{
+			timer += 0.01;
+		}
+	}
+
+
 	void OnGUI() {
 		
 		
 		if(trigger == 1)	{
-			GUI.DrawTexture( new Rect(400, 600, 700, 70), text_box);
-			GUI.Label ( new Rect (500, 600,700,70), "Enough lazing about boy!", boo_font);
+			GUI.DrawTexture( new Rect(440, 600, 700, 70), text_box);
+			GUI.Label ( new Rect (525, 600,700,70), "Enough lazing about boy!", boo_font);
 		}
 		else if(trigger == 2){
-			GUI.DrawTexture ( new Rect(350, 600, 700, 70), text_box);
-			GUI.Label ( new Rect (360, 600,700,70), "Don't you want your candy back?", boo_font);
+			GUI.DrawTexture ( new Rect(420, 600, 760, 70), text_box);
+			GUI.Label ( new Rect (465, 600,700,70), "Don't you want your candy back?", boo_font);
 		}
 		else if(trigger == 3){
-			GUI.DrawTexture ( new Rect(350, 600, 700, 70), text_box);
-			GUI.Label ( new Rect (550, 600,700,70), "Have at them!", boo_font);
+			GUI.DrawTexture ( new Rect(600, 600, 400, 70), text_box);
+			GUI.Label ( new Rect (650, 600,700,70), "Have at them!", boo_font);
 		}
 		else if(trigger == 12){
-			GUI.DrawTexture ( new Rect(150, 600, 900, 70), text_box_2);
-			GUI.Label ( new Rect (300, 600, 900, 70), "Such a kind gesture from a kind young man!", boo_font);
+			beginTimer ();
+			GUI.DrawTexture ( new Rect(320, 600, 950, 70), text_box_2);
+			GUI.Label ( new Rect (350, 600, 900, 70), "Such a kind gesture from a kind young man!", boo_font);
 		}
 		else if(trigger == 13)	{
-			GUI.DrawTexture ( new Rect(350, 600, 900, 70), text_box_2);
-			GUI.Label (new Rect (500, 600, 900, 70), "That's not nearly enough to satisfy me!", boo_font);
+			beginTimer ();
+			GUI.DrawTexture ( new Rect(370, 600, 900, 70), text_box_2);
+			GUI.Label (new Rect (420, 600, 900, 70), "That's not nearly enough to satisfy me!", boo_font);
 		}
 		else if(trigger == 15)	{
-			GUI.DrawTexture ( new Rect(300, 600, 900, 70), text_box_2);
-			GUI.Label (new Rect (360, 600, 750, 70), "Well done! You've earned a brief respite.", boo_font);
+			beginTimer ();
+			GUI.DrawTexture ( new Rect(370, 600, 900, 70), text_box_2);
+			GUI.Label (new Rect (400, 600, 750, 70), "Well done! You've earned a brief respite.", boo_font);
 		}
 		else if(trigger == 16)	{
-			GUI.DrawTexture ( new Rect(250, 600, 900, 70), text_box_2);
-			GUI.Label (new Rect (325, 600, 750, 70), "Ready your arms, here they come!", boo_font);
+			beginTimer ();
+			GUI.DrawTexture ( new Rect(370, 600, 900, 70), text_box_2);
+			GUI.Label (new Rect (475, 600, 750, 70), "Ready your arms, here they come!", boo_font);
 		}
 		
 		
@@ -65,6 +82,14 @@ public class TalkToBoo : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
+
+		print(timer);
+
+		if(timer >= 3000 && trigger > 10)	{
+			trigger = 0;
+			isTiming = false;
+		}
+
 		var ui = GameObject.Find ("Main Camera");
 		UI ui_script = ui.GetComponent<UI>();
 		
